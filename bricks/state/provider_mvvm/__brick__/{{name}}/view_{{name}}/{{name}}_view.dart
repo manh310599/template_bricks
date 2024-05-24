5b2428bcf699832219dd9d1b9d306101b9f1d02a
manh310599 <manhmanh310599@gmail.com>
2024-05-24 15:05:30 +0700

    mvvm_provider

---
+++ bricks/state/provider_mvvm/__brick__/{{name}}/view_{{name}}/{{name}}_view.dart
@@ -0,0 +1,9 @@
+class {{name.pascalCase()}} extends StatelessWidget {
+     @override
+  Widget build(BuildContext context) {
+    return Text(
+      // Don't forget to pass the type of the object you want to obtain to `watch`!
+      context.watch<String>(),
+    );
+  }
+}
---
+++ bricks/state/provider_mvvm/__brick__/{{name}}/viewmodel_{{name}}/viewmodel_{{name}}.dart
@@ -0,0 +1,5 @@
+class {{name.pascalCase()}} extends ChangeNotifier {
+    init(){
+        
+    }
+}
